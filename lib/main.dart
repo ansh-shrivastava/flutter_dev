7dab321ac7be2699e9bf49a3e03314176dbbe1c3
ansh-shrivastava <anshshrivastava777@gmail.com>
2026-02-24 03:12:17 +0530

    gesture decator

--- lib/main.dart
+++ lib/main.dart
@@ -16,45 +16,18 @@ class MyApp extends StatelessWidget {
         backgroundColor: const Color.fromARGB(255, 190, 87, 208),
         appBar: AppBar(backgroundColor: Colors.grey),
 
-        body: ListView(
-          // make the app scrollable
-          scrollDirection: Axis.horizontal,
-          children: [
-            Container(
-              height: 600,
-              width: 350,
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              print("tap detected");
+            },
+            child: Container(
+              height: 100,
+              width: 100,
               color: Colors.amber,
-              padding: EdgeInsets.all(25),
-              child: Center(child: Text("this is the child widget")),
+              child: Text("tap me "),
             ),
-            Container(
-              height: 600,
-              width: 350,
-              decoration: BoxDecoration(
-                color: Colors.deepPurpleAccent,
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(25),
-              child: Center(
-                child: Text(
-                  "this is the second container",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-
-            Container(
-              height: 600,
-              width: 350,
-              color: Colors.amber,
-              padding: EdgeInsets.all(25),
-              child: Icon(Icons.favorite, size: 40, color: Colors.black),
-            ),
-          ],
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
