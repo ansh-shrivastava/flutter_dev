c987917d9a3c00c0014b1936109d07e9ce4b1c50
ansh-shrivastava <anshshrivastava777@gmail.com>
2026-02-23 16:22:43 +0530

    Initial Flutter project setup

---
+++ lib/pages/first_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial/pages/second_page.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("this is the first page hellow ")),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "home"),
+          BottomNavigationBarItem(icon: Icon(Icons.upload), label: "upload"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "profile"),
+        ],
+      ),
+    );
+  }
+}
