5af999ae9f683b70ce0e76f364d9c14ff5a5f897
ansh-shrivastava <anshshrivastava777@gmail.com>
2026-02-24 02:38:21 +0530

    columns/rows and expanded

--- lib/main.dart
+++ lib/main.dart
@@ -13,44 +13,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 190, 87, 208),
-        body: Center(
-          child: Column(
-            children: [
-              Container(
-                height: 100,
-                width: 100,
-                color: Colors.amber,
-                padding: EdgeInsets.all(25),
-                child: Center(child: Text("this is the child widget")),
+        appBar: AppBar(backgroundColor: Colors.grey),
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              color: Colors.amber,
+              padding: EdgeInsets.all(25),
+              child: Center(child: Text("this is the child widget")),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                color: Colors.deepPurpleAccent,
+                borderRadius: BorderRadius.circular(20),
               ),
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  color: Colors.deepPurpleAccent,
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                padding: EdgeInsets.all(25),
-                child: Center(
-                  child: Text(
-                    "this is the second container",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+              padding: EdgeInsets.all(25),
+              child: Center(
+                child: Text(
+                  "this is the second container",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
-              Container(
-                height: 100,
+            ),
+            Expanded(
+              flex: 2, // maek this relativly expanded
+              // fills up the space
+              child: Container(
                 width: 100,
                 color: Colors.amber,
                 padding: EdgeInsets.all(25),
                 child: Icon(Icons.favorite, size: 40, color: Colors.black),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
       debugShowCheckedModeBanner: false,
